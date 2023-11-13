70cc09d21f222f108ca5b492d714dcdc187e0a23
Diego Pérez <diegoalejandroperezm@gmail.com>
2023-11-12 22:48:05 -0500

    Se cambia paleta de colores, se agrega logo personalizado y se agrega información a la sesion home y sobre mi

--- assets/js/app.js
+++ assets/js/app.js
@@ -57,7 +57,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // -------- typed js -----
 const typed = new Typed('.multiple-text', {
-    strings: ['Fronted Developer'],
+    strings: ['Desarrollador de software', 'Desarrollador Frontend'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
