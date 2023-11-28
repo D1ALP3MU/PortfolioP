deb842f1ab8475a58835c56c1ac63ff95813eaef
Diego Pérez <diegoalejandroperezm@gmail.com>
2023-11-27 22:58:50 -0500

    Se le da funcionalidad al formulario de contacto, se agrega librería sweetAlert2 y se dan nuevos estilos

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,3 +62,97 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 })
+
+// -------- smtp js (Para enviar email)-----
+const form = document.querySelector('form');
+const fullName = document.querySelector('#name');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone');
+const subject = document.querySelector('#subject');
+const message = document.querySelector('#message');
+
+function sendEmail() {
+    const bodyMessage = `Full name: ${fullName.value}.<br> Email: ${email.value}.<br> Phone Number: ${phone.value}.<br> Message: ${message.value}.`
+
+    Email.send({
+        SecureToken: "866c3aa3-7678-4424-8bfa-09da6661a174",
+        To : 'diegoalejandroperezm@gmail.com',
+        From : "diegoalejandroperezm@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+        message => {
+            if (message === 'OK') {
+                Swal.fire({
+                    title: "Bien hecho!",
+                    text: "Mensaje enviado con éxito!",
+                    icon: "success"
+                });
+            }
+        }
+    );
+}
+
+function checkInputs() {
+    const items = document.querySelectorAll('.item');
+
+    for (const item of items) {
+        if (item.value === "") {
+            item.classList.add('error');
+            item.parentElement.classList.add('error');
+        }
+
+        if (items[1].value !== "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener('keyup', () => {
+            checkEmail();
+        });
+
+        item.addEventListener('keyup', () => {
+            if (item.value !== "") {
+                item.classList.remove('error');
+                item.parentElement.classList.remove('error');
+            } else {
+                item.classList.add('error');
+            item.parentElement.classList.add('error');
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+    //const emailRegex = "^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$";
+    //const emailRegex = [a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5};
+
+    const errorTxtEmail = document.querySelector('.error-txt.email');
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add('error');
+        email.parentElement.classList.add('error');
+
+        if (email.value !== "") {
+            errorTxtEmail.innerText = 'Ingrese un correo electrónico válido'
+        } else {
+            errorTxtEmail.innerText = 'El correo no puede estar en blanco'
+        }
+    } else {
+        email.classList.remove('error');
+        email.parentElement.classList.remove('error');
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    checkInputs();
+
+    if (!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !message.classList.contains("error")) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
+
+});
